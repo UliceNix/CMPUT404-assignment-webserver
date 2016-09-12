24b0498fafc8396b342c5a61efa8cda08f1840c0
Alice Wu <AliceWu@Alices-MacBook-Pro.local>
2016-09-12 08:18:10 -0600

    first pass: lousy code quality.

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-#  coding: utf-8 
+#  coding: utf-8
+import inspect
+import os.path
 import SocketServer
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2013 Abram Hindle, Alice Wu, Eddie Antonio Santos
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +30,61 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+    __message501 = "HTTP/1.1 501 Not Implemented\n" \
+    "Content-Type text/html\n\n" \
+    "<!DOCTYPE html>\n" \
+    "<html><body>HTTP/1.1 501 Not Implemented</b>" \
+    "Server only supports GET.</body></html>"
+
     def handle(self):
+        self.message = ""
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        self.__extractRequestData(self.data)
+
+        # Only allow GET method
+        if (self.__requestMethod != "GET"):
+            self.message = self.__message501
+        elif (self.__pathValidator(self.__requestFilePath)):
+            self.__requestFileType = self.__requestFilePath.split(".")[-1].lower()
+            if self.__requestFileType in ["html", "css"]:
+                self.message += ("HTTP/1.1 200 OK\n"
+                                + "Content-Type: text/" + self.__requestFileType + "\n\n"
+                                + open(self.__requestFilePath).read())
+            else:
+                self.message += ("HTTP/1.1 200 OK\n"
+                                + "Content-Type: text/plain\n\n"
+                                + open(self.__requestFilePath).read())
+        elif (self.__pathValidator(self.__requestFilePath + "/index.html")):
+            self.message += ("HTTP/1.1 200 OK\n"
+                            + "Content-Type: text/html\n\n"
+                            + open(self.__requestFilePath+"/index.html").read())
+        else:
+            self.message += ("HTTP/1.1 404 Not Found\n"+
+                        "Content-Type: text/html\n\n"+
+                        "<!DOCTYPE html>\n"+
+                        "<html><body>HTTP/1.1 404 Not Found</b>"+
+                        "File not found on server directory.</body></html>")
+        self.request.sendall(self.message)
+
+
+    def __extractRequestData(self, request):
+        self.__request = request.splitlines()
+        self.__requestData = self.__request[0].split() \
+                             if (self.__request) else []
+
+        if (self.__requestData):
+            self.__requestMethod = self.__requestData[0].upper()
+            self.__currentDir = os.path.dirname(
+                                os.path.abspath(
+                                inspect.getfile(inspect.currentframe())))
+            self.__requestFilePath = self.__currentDir + "/www" + self.__requestData[1] if (len(self.__requestData) > 1 and self.__requestData[1]) else ""
+        else:
+            self.__requestMethod = ""
+            self.__currentDir = ""
+            self.__requestFilePath = ""
+
+    def __pathValidator(self, path):
+        return os.path.isfile(path) and self.__currentDir in os.path.realpath(path)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
